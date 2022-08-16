d927d327bf699d9cb1942a8b590d1d6b282d4e18
Sank <71709931+Sank34@users.noreply.github.com>
2022-08-16 10:05:10 +0300

    test

---
+++ system.js
@@ -0,0 +1,31 @@
+/* ServoController alpha 0.0.1
+ * This is the file of all the button's functions
+ * and the system for them in order to fully work
+ * This is a to-do file *
+*/
+
+//elements selected
+let registerbtn = document.querySelector("div.content > div.footer > div.btn")
+let resetposbtn = document.querySelector("div.content > div.footer > div.btn")
+let clearallbtn = document.querySelector("div.content > div.footer > div.btn")
+let saveposbtn = document.querySelector("div.content > div.footer > div.btn")
+//register positions
+function registerpos() {
+
+}
+
+//reset positions
+function resetpos() {
+
+}
+
+//clearall positions
+function clearallpos() {
+
+}
+
+//save position
+function savepos(){
+
+}
+
